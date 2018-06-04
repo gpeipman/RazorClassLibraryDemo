4a90be8139ae4ead3696a19db2805ac644ec1a26
Gunnar <gpeipman@hotmail.com>
2018-06-04 23:13:49 +0300

    razor class library with mvc and views overriding

---
+++ RazorClassLibraryDemo/RazorClassLibraryDemo.Lib/Areas/MyFeature/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RazorClassLibraryDemo.Lib.Areas.MyFeature.Controllers
+{
+    [Area("MyFeature")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult OverrideExample()
+        {
+            return View();
+        }
+    }
+}
